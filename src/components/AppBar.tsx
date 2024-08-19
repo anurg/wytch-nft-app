216f2d0bccaf1b7aaa930b926c0b2193c03514fd
anurg <anurg@yahoo.com>
2024-08-19 14:04:22 +0530

    Edited Magic Eden link.

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,79 +1,108 @@
-import { FC } from 'react';
+import { FC } from "react";
 import Link from "next/link";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import React, { useState } from "react";
-import { useAutoConnect } from '../contexts/AutoConnectProvider';
-import NetworkSwitcher from './NetworkSwitcher';
-import NavElement from './nav-element';
-import me from '../assets/magiceden.png';
+import { useAutoConnect } from "../contexts/AutoConnectProvider";
+import NetworkSwitcher from "./NetworkSwitcher";
+import NavElement from "./nav-element";
+import me from "../assets/magiceden.png";
 
 const meL = () => {
-	window.open("https://magiceden.io/marketplace/witchezz_frenzy");
-}
+  window.open("https://magiceden.io");
+};
 
 const tg = () => {
-	window.open("https://t.me/witchonsolana");
-}
+  window.open("https://t.me/witchonsolana");
+};
 
 const tweet = () => {
-	window.open("https://x.com/WitchezzFrenzy");
-}
+  window.open("https://x.com/WitchezzFrenzy");
+};
 
 const home = () => {
-	window.open("https://www.witchezzfrenzy.com/");
-}
+  window.open("https://www.witchezzfrenzy.com/");
+};
 
 const WalletMultiButtonDynamic = dynamic(
-  async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
   { ssr: false }
 );
 
 export const AppBar: React.FC = () => {
   const { autoConnect, setAutoConnect } = useAutoConnect();
   const [isNavOpen, setIsNavOpen] = useState(false);
-  
+
   return (
-    <div >
+    <div>
       {/* NavBar / Header */}
       <div className="navbar flex flex-row md:mb-2 shadow-lg bg-black text-neutral-content border-b border-zinc-600 bg-opacity-66">
         <div className="navbar-start align-items-center">
           <div className="hidden sm:inline w-22 h-22 md:p-2 ml-10">
-          <img className='logo' onClick={() => window.location.href = '/'} src="https://witch-8vg.pages.dev/imgs/witch.jpg"/>
-           </div>
-           {/* <div className='mynft'> Solana Devnet Version(for Testing)</div> */}
+            <img
+              className="logo"
+              onClick={() => (window.location.href = "/")}
+              src="https://witch-8vg.pages.dev/imgs/witch.jpg"
+            />
+          </div>
+          {/* <div className='mynft'> Solana Devnet Version(for Testing)</div> */}
           <WalletMultiButtonDynamic className="btn-ghost btn-sm relative flex md:hidden text-lg " />
-
         </div>
 
         {/* Nav Links */}
         {/* Wallet & Settings */}
         <div className="navbar-end">
-        
           <div className="hidden md:inline-flex align-items-center justify-items gap-6">
-          <img className='me' onClick={meL} src="https://optimusassets.pages.dev/imgs/magiceden.png"/>
-           <img className='me' src="https://social-c3e.pages.dev/imgs/twitter (1).png" onClick={tweet}/>
-          <img className='me' src="https://social-op.pages.dev/imgs/telegram.png" onClick={tg}/>
-          <img className='me' src="https://cdn-icons-png.flaticon.com/128/10003/10003015.png" onClick={home}/>
-          <div className='mynft' onClick={() => window.location.href = 'mynft'}>MY NFTs</div>
-          
-          <WalletMultiButtonDynamic className="btn-ghost btn-sm rounded-btn text-lg mr-6 " />
+            <img
+              className="me"
+              onClick={meL}
+              src="https://optimusassets.pages.dev/imgs/magiceden.png"
+            />
+            <img
+              className="me"
+              src="https://social-c3e.pages.dev/imgs/twitter (1).png"
+              onClick={tweet}
+            />
+            <img
+              className="me"
+              src="https://social-op.pages.dev/imgs/telegram.png"
+              onClick={tg}
+            />
+            <img
+              className="me"
+              src="https://cdn-icons-png.flaticon.com/128/10003/10003015.png"
+              onClick={home}
+            />
+            <div
+              className="mynft"
+              onClick={() => (window.location.href = "mynft")}
+            >
+              MY NFTs
+            </div>
 
-          
-        </div>
-  
-        <div className="dropdown dropdown-end">
-           
-          <ul tabIndex={0} className="p-2 shadow menu dropdown-content bg-base-100 rounded-box sm:w-52">
-            <li>
-              <div className="form-control bg-opacity-100">
-                <label className="cursor-pointer label">
-                  <a>Autoconnect</a>
-                  <input type="checkbox" checked={autoConnect} onChange={(e) => setAutoConnect(e.target.checked)} className="toggle" />
-                </label>
-              </div>
-            </li>
-          </ul>
-        </div>
+            <WalletMultiButtonDynamic className="btn-ghost btn-sm rounded-btn text-lg mr-6 " />
+          </div>
+
+          <div className="dropdown dropdown-end">
+            <ul
+              tabIndex={0}
+              className="p-2 shadow menu dropdown-content bg-base-100 rounded-box sm:w-52"
+            >
+              <li>
+                <div className="form-control bg-opacity-100">
+                  <label className="cursor-pointer label">
+                    <a>Autoconnect</a>
+                    <input
+                      type="checkbox"
+                      checked={autoConnect}
+                      onChange={(e) => setAutoConnect(e.target.checked)}
+                      className="toggle"
+                    />
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
